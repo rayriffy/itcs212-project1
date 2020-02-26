36d429641cd4116a6549dbeb8c42311f734bb8a3
Phumrapee Limpianchop <git@rayriffy.com>
2020-02-26 16:48:41 +0700

    style: lint

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -7,23 +7,26 @@ window.addEventListener("DOMContentLoaded", () => {
       const entrites = Array.from(inputs).map(input => {
         return [input.id, input.value];
       });
-      
-      if(entrites.filter(([_key, val]) => val.trim() === "").length > 0) {
-        document.querySelector('.submit > .error').innerText = '*Please fill required fields'
+
+      if (entrites.filter(([_key, val]) => val.trim() === "").length > 0) {
+        document.querySelector(".submit > .error").innerText =
+          "*Please fill required fields";
       } else {
         const res = entrites.reduce(
           (acc, [key, val]) => ({ ...acc, [key]: val }),
           {}
         );
-        
+
         // TODO: Submit data to server
-        console.log('data: ', res)
-        
-        document.querySelector('div.content > div.flex > h1').innerText = 'Sent'
-        document.querySelector('div.content > div.form').innerHTML = ''
-        document.querySelector('div.content > span').remove()
-        document.querySelector('div.content > .submit').remove()
-        document.querySelector('div.content > p').innerText = 'Message has been sent to recipiant. We will reply back to you in a few business days.'
+        console.log("data: ", res);
+
+        document.querySelector("div.content > div.flex > h1").innerText =
+          "Sent";
+        document.querySelector("div.content > div.form").innerHTML = "";
+        document.querySelector("div.content > span").remove();
+        document.querySelector("div.content > .submit").remove();
+        document.querySelector("div.content > p").innerText =
+          "Message has been sent to recipiant. We will reply back to you in a few business days.";
       }
     });
 });
